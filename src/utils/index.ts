75bb20e8bbcbea9cf89ede1fd459bc2ab557e3ef
Stephano <stephan.orgiazzi@gmail.com>
2022-11-17 16:04:51 +0100

    add filters component

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,16 @@ export const convertTime = (time: number) => {
     const minutes = time % 60
     return `${hours}h ${minutes}min`
 }
+
+export const genreOptions = [
+    { value: '', name: 'All' },
+    { value: '28', name: 'Action' },
+    { value: '12', name: 'Adventure' },
+    { value: '35', name: 'Comedy' },
+    { value: '18', name: 'Drama' },
+    { value: '36', name: 'History' },
+    { value: '10749', name: 'Romance' },
+    { value: '878', name: 'Sci-Fi' },
+    { value: '53', name: 'Thriller' },
+    { value: '10752', name: 'War' }
+]
