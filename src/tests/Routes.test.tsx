6f264cebc58959660584b03e9a908b8dbec22cf9
Stephano <stephan.orgiazzi@gmail.com>
2022-11-17 15:34:52 +0100

    add tests for routes

---
+++ src/tests/Routes.test.tsx
@@ -0,0 +1,52 @@
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { ApiProvider } from '@reduxjs/toolkit/query/react'
+import store from '../features/store'
+import { apiSlice } from '../features/api/apiSlice'
+import Movies from '../pages/Movies/Movies'
+import Shows from '../pages/Shows/Shows'
+import Root from '../pages/Root/Root'
+
+describe('Movie route and Show route', () => {
+    it('should render a header element', () => {
+        render(
+            <Provider store={store}>
+                <ApiProvider api={apiSlice}>
+                    <MemoryRouter>
+                        <Root />
+                    </MemoryRouter>
+                </ApiProvider>
+            </Provider>
+        )
+        expect(screen.getByRole('heading')).toBeInTheDocument()
+    })
+
+    it('should render a list', async () => {
+        render(
+            <Provider store={store}>
+                <ApiProvider api={apiSlice}>
+                    <MemoryRouter>
+                        <Movies />
+                    </MemoryRouter>
+                </ApiProvider>
+            </Provider>
+        )
+        const list = await screen.findByTestId('flicks-list')
+        expect(list).toBeInTheDocument()
+    })
+
+    it('should render a list', async () => {
+        render(
+            <Provider store={store}>
+                <ApiProvider api={apiSlice}>
+                    <MemoryRouter>
+                        <Shows />
+                    </MemoryRouter>
+                </ApiProvider>
+            </Provider>
+        )
+        const list = await screen.findByTestId('flicks-list')
+        expect(list).toBeInTheDocument()
+    })
+})
