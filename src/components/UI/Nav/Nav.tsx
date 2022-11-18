16014ce4ba339ff8795fdd5511008ef6eb79b5f8
Stephano <stephan.orgiazzi@gmail.com>
2022-11-18 16:13:13 +0100

    improve nav styles

--- src/components/UI/Nav/Nav.tsx
+++ src/components/UI/Nav/Nav.tsx
@@ -12,19 +12,22 @@ function Nav() {
             </Link>
             <ul>
                 <li>
-                    <NavLink to="movie" style={({ isActive }) => (isActive ? { color: '#fff' } : { color: '#545e6f' })}>
+                    <NavLink
+                        to="movie"
+                        style={({ isActive }) => (isActive ? { color: '#f5f5f5' } : { color: '#545e6f' })}
+                    >
                         Movies
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to="tv" style={({ isActive }) => (isActive ? { color: '#fff' } : { color: '#545e6f' })}>
+                    <NavLink to="tv" style={({ isActive }) => (isActive ? { color: '#f5f5f5' } : { color: '#545e6f' })}>
                         TV Shows
                     </NavLink>
                 </li>
                 <li>
                     <NavLink
                         to="watchlist"
-                        style={({ isActive }) => (isActive ? { color: '#fff' } : { color: '#545e6f' })}
+                        style={({ isActive }) => (isActive ? { color: '#f5f5f5' } : { color: '#545e6f' })}
                     >
                         Watchlist
                     </NavLink>
