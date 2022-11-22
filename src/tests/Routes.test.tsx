f24a17d882d60b95318101a3ffa0647c8795eaa8
Stephano <stephan.orgiazzi@gmail.com>
2022-11-22 12:29:07 +0100

    fix tests

--- src/tests/Routes.test.tsx
+++ src/tests/Routes.test.tsx
@@ -3,13 +3,13 @@ import { MemoryRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { ApiProvider } from '@reduxjs/toolkit/query/react'
 import store from '../store'
-import { apiSlice } from '../features/api/apiSlice'
+import { apiSlice } from '../store/apiSlice'
 import Movies from '../pages/Movies/Movies'
 import Shows from '../pages/Shows/Shows'
 import Root from '../pages/Root/Root'
 
 describe('Movie route and Show route', () => {
-    it('should render a header element', () => {
+    it('should render a nav element', () => {
         render(
             <Provider store={store}>
                 <ApiProvider api={apiSlice}>
@@ -19,7 +19,7 @@ describe('Movie route and Show route', () => {
                 </ApiProvider>
             </Provider>
         )
-        expect(screen.getByRole('heading')).toBeInTheDocument()
+        expect(screen.getByRole('navigation')).toBeInTheDocument()
     })
 
     it('should render a list', async () => {
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
+/// <reference types="vitest" />
 export default defineConfig({
     plugins: [react()],
     test: {
