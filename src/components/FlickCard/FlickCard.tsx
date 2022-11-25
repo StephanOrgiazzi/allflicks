115f4fbcdb56ac3d0a09a4b977a50a490f8d3805
Stephano <stephan.orgiazzi@gmail.com>
2022-11-25 16:18:09 +0100

    remove blurry effect on lazyloading

--- src/components/FlickCard/FlickCard.tsx
+++ src/components/FlickCard/FlickCard.tsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom'
 import { Flick } from '../../types'
 import { LazyLoadImage } from 'react-lazy-load-image-component'
-import 'react-lazy-load-image-component/src/effects/blur.css'
 import styles from './FlickCard.module.scss'
 
 const FlickCard = ({ id, title, poster_path: cover, type, year }: Flick) => {
@@ -11,14 +10,7 @@ const FlickCard = ({ id, title, poster_path: cover, type, year }: Flick) => {
         <Link to={`/${type}/${id}`}>
             <li className={styles['flick-card']}>
                 <figure>
-                    <LazyLoadImage
-                        src={url}
-                        alt={`Poster of ${title}`}
-                        width={150}
-                        height={225}
-                        //placeholder={<h1>Image taken from https://via.placeholder.com/150</h1>}
-                        effect="blur"
-                    />
+                    <LazyLoadImage src={url} alt={`Poster of ${title}`} width={150} height={225} />
                     <figcaption>{title}</figcaption>
                 </figure>
             </li>
