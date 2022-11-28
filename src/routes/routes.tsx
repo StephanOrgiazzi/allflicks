ff04d1afc46d62c09716bb85a4e8adc0056216ea
Stephano <stephan.orgiazzi@gmail.com>
2022-11-28 13:59:08 +0100

    add lazyloading and body background image

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,19 +1,50 @@
+import React from 'react'
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
 import Root from './pages/Root/Root'
-import Error from './pages/Error/Error'
-import Movies from './pages//Movies/Movies'
-import Shows from './pages/Shows/Shows'
-import WatchList from './pages/WatchList/WatchList'
-import FlickPage from '../components/FlickPage/FlickPage'
+import Loader from '../components/UI/Loader/Loader'
+
+const Movies = React.lazy(() => import('./pages/Movies/Movies'))
+const Shows = React.lazy(() => import('./pages/Shows/Shows'))
+const WatchList = React.lazy(() => import('./pages/WatchList/WatchList'))
+const FlickPage = React.lazy(() => import('../components/FlickPage/FlickPage'))
+const Error = React.lazy(() => import('./pages/Error/Error'))
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<Root />} errorElement={<Error />}>
             <Route errorElement={<Error />}>
-                <Route path="/movie/" element={<Movies />} />
-                <Route path="/tv/" element={<Shows />} />
-                <Route path="/:flickType/:flickId" element={<FlickPage />} />
-                <Route path="/watchlist/" element={<WatchList />} />
+                <Route
+                    path="/movie/"
+                    element={
+                        <React.Suspense fallback={<Loader />}>
+                            <Movies />
+                        </React.Suspense>
+                    }
+                />
+                <Route
+                    path="/tv/"
+                    element={
+                        <React.Suspense fallback={<Loader />}>
+                            <Shows />
+                        </React.Suspense>
+                    }
+                />
+                <Route
+                    path="/:flickType/:flickId"
+                    element={
+                        <React.Suspense fallback={<Loader />}>
+                            <FlickPage />
+                        </React.Suspense>
+                    }
+                />
+                <Route
+                    path="/watchlist/"
+                    element={
+                        <React.Suspense fallback={<Loader />}>
+                            <WatchList />
+                        </React.Suspense>
+                    }
+                />
             </Route>
         </Route>
     )
