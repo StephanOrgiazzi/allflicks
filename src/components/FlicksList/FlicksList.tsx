582b1529bd294362fb7b74773f1b81861915f783
Stephano <stephan.orgiazzi@gmail.com>
2022-12-05 09:34:52 +0100

    improve FlicksList code quality and remove duplicate test

--- src/components/FlicksList/FlicksList.tsx
+++ src/components/FlicksList/FlicksList.tsx
@@ -10,7 +10,7 @@ import styles from './FlicksList.module.scss'
 function FlicksList({ type, list }: { type?: string; list?: FlickElement[] }) {
     const [page, setPage] = useState(1)
     const [genre, setGenre] = useState('all')
-    const [flickType] = useState(type)
+    const flickType = type
 
     const {
         data: flicks,
@@ -36,24 +36,22 @@ function FlicksList({ type, list }: { type?: string; list?: FlickElement[] }) {
 
     return (
         <>
-            {!isLoading && (
-                <section className={styles['flicks-list']}>
-                    {!list && <Filter flickType={flickType} genre={genre} setGenre={setGenre} />}
-                    <ul data-testid="data-flicks-list">
-                        {flickList?.map((item: Flick) => (
-                            <FlickCard
-                                key={item.id}
-                                id={item.id}
-                                title={item.title ?? item.name}
-                                poster_path={item.poster_path}
-                                type={flickType || item.type}
-                                year={item.year}
-                            />
-                        ))}
-                    </ul>
-                    {!list && <Pagination setPage={setPage} hasPrev={page > 1} />}
-                </section>
-            )}
+            <section className={styles['flicks-list']}>
+                {!list && <Filter flickType={flickType} genre={genre} setGenre={setGenre} />}
+                <ul data-testid="data-flicks-list">
+                    {flickList?.map((item: Flick) => (
+                        <FlickCard
+                            key={item.id}
+                            id={item.id}
+                            title={item.title ?? item.name}
+                            poster_path={item.poster_path}
+                            type={flickType || item.type}
+                            year={item.year}
+                        />
+                    ))}
+                </ul>
+                {!list && <Pagination setPage={setPage} hasPrev={page > 1} />}
+            </section>
             {isError && <p>Error: please try again later.</p>}
             {!isError && list?.length === 0 && <p>Your watchlist is empty</p>}
             {isLoading && <Loader />}
